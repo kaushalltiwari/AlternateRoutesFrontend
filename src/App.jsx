a9a71108c3a79713b858bb96086fc9875068b154
Hitman <kaushalltiwariii@gmail.com>
2024-06-22 20:42:03 +0530

    Before DropDown

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
-import MiddleSection from './middleSection.jsx'
+import SearchArea from './SearchArea/searchArea.jsx'
+import Header from './header.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <MiddleSection></MiddleSection>
+      <Header></Header>
+      <SearchArea></SearchArea>
     </>
   )
 }
