d5e5f84d0e9bd64e584666b5ca884493d0514ce4
Hitman <kaushalltiwariii@gmail.com>
2024-06-29 16:02:34 +0530

    Date Picker Added

--- src/Components/searchAreaDesktop/searchAreaDesktop.jsx
+++ src/Components/searchAreaDesktop/searchAreaDesktop.jsx
@@ -1,49 +1,68 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import './searchAreaDesktop.css'
+import "flatpickr/dist/themes/material_green.css";
+import Flatpickr from "react-flatpickr";
+import './FlatpickrCustom.css'
+
 
 export default function searchAreaDesktop() {
+  
+  const [date, setDate] = useState(new Date());
   return (
-    <div class="container mt-5">
-          <div class="card mb-3">
-              <div class="card-body">
+    <div className="container mt-5">
+          <div className="card mb-3">
+              <div className="card-body">
                   This is some text within a card body.
               </div>
           </div>
-          <div class="card mb-3">
-              <div class="card-body pt-5">
-                  <div class="card">
-                      <div class="card-body">
-                          <div class="container">
-                              <div class="row align-items-end">
-                                  <div class="col-12 col-md-4 border-end queryInfo">
-                                      <div class="dropdown">
-                                          <div class="d-flex flex-column" role="button" data-bs-toggle="dropdown">
-                                              <p class="mb-n5">From</p>
-                                              <h3 class="mt-n5">Delhi</h3>
+          <div className="card mb-3">
+              <div className="card-body pt-5">
+                  <div className="card">
+                      <div className="card-body">
+                          <div className="container">
+                              <div className="row align-items-end">
+                                  <div className="col-12 col-md-4 border-end queryInfo">
+                                      <div className="dropdown">
+                                          <div className="d-flex flex-column" role="button" data-bs-toggle="dropdown">
+                                              <p className="mb-n5">From</p>
+                                              <h3 className="mt-n5">Delhi</h3>
                                               <p>New Delhi Railway Station</p>
                                           </div>
-                                          <ul class="dropdown-menu w-100 drp">
-                                              <li><a class="dropdown-item" href="#">Action</a></li>
-                                              <li><a class="dropdown-item" href="#">Another action</a></li>
-                                              <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                          <ul className="dropdown-menu w-100 drp">
+                                              <li><a className="dropdown-item" href="#">Action</a></li>
+                                              <li><a className="dropdown-item" href="#">Another action</a></li>
+                                              <li><a className="dropdown-item" href="#">Something else here</a></li>
                                           </ul>
                                       </div>
                                   </div>
-                                  <div class="col-12 col-md-4 border-end">
-                                      <div class="d-flex flex-column">
+                                  <div className="col-12 col-md-4 border-end">
+                                      <div className="d-flex flex-column">
                                           <p>From</p>
                                           <h3>Delhi</h3>
                                           <p>New Delhi Railway Station</p>
                                       </div>
                                   </div>
-                                  <div class="col-12 col-md-2 border-end">
-                                      <div class="d-flex flex-column">
-                                          <p>From</p>
-                                          <h3>Delhi</h3>
-                                          <p>New Delhi Railway Station</p>
+                                  <div className="col-12 col-md-2 border-end">
+                                      <div className="d-flex flex-column">
+                                          <p>Travel Dates</p>
+                                          {/* <Calendar onChange={setDate} value={date} /> */}
+                                          <Flatpickr
+                                                data-enable-time
+                                                value={date}
+                                                onChange={([selectedDate]) => {
+                                                setDate(selectedDate);
+                                                }}
+                                                options={{
+                                                    dateFormat: 'Y-m-d',
+                                                    enableTime: false,
+                                                    minDate: 'today',
+                                                    maxDate: new Date().fp_incr(120)
+                                                }}
+                                        />
                                       </div>
                                   </div>
-                                  <div class="col-12 col-md-2">
-                                      <div class="d-flex flex-column">
+                                  <div className="col-12 col-md-2">
+                                      <div className="d-flex flex-column">
                                           <p>From</p>
                                           <h3>Delhi</h3>
                                           <p>New Delhi Railway Station</p>
