5373f6243ba86c5db9d4fc9b5451d7a0872173fb
Hitman <kaushalltiwariii@gmail.com>
2024-06-30 17:51:34 +0530

    Styles Added

--- src/Components/searchAreaDesktop/searchAreaDesktop.jsx
+++ src/Components/searchAreaDesktop/searchAreaDesktop.jsx
@@ -26,7 +26,7 @@ export default function searchAreaDesktop() {
            var position = $('#Test').offset();
            $('.flatpickr-calendar').addClass('arrowTop arrowLeft open')
            $(".flatpickr-calendar").css({
-                top: position.top + 150,
+                top: position.top + 120,
                 left: position.left,
                 display: 'block'
            });
@@ -49,7 +49,7 @@ export default function searchAreaDesktop() {
                   This is some text within a card body.
               </div>
           </div>
-          <div className="card mb-3">
+          <div className="card mb-3" id="wholeSearchArea">
               <div className="card-body pt-5">
                   <div className="card">
                       <div className="card-body" id="parent_cardBody">
@@ -59,29 +59,40 @@ export default function searchAreaDesktop() {
                                       <div className="dropdown">
                                           <div className="d-flex flex-column" role="button" data-bs-toggle="dropdown">
                                               <p className="topText">From</p>
-                                              <h3 className="middleText">Delhi</h3>
-                                              <p>New Delhi Railway Station</p>
+                                              <p className="middleText fs-2 fw-semibold">Delhi</p>
+                                              <p className="dayText">New Delhi Railway Station</p>
                                           </div>
                                           <ul className="dropdown-menu w-100 drp">
-                                              <li><a className="dropdown-item" href="#">Action</a></li>
-                                              <li><a className="dropdown-item" href="#">Another action</a></li>
-                                              <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                              <li><a className="dropdown-item" href="#">Delhi</a></li>
+                                              <li><a className="dropdown-item" href="#">Mumbai</a></li>
+                                              <li><a className="dropdown-item" href="#">Kolkata</a></li>
+                                              <li><a className="dropdown-item" href="#">Asansol</a></li>
+                                              <li><a className="dropdown-item" href="#">Dhanbad</a></li>
                                           </ul>
                                       </div>
                                   </div>
-                                  <div className="col-12 col-md-4 border-end">
-                                      <div className="d-flex flex-column topText">
-                                          <p>From</p>
-                                          <h3 className="middleText" >Delhi</h3>
-                                          <p>New Delhi Railway Station</p>
+                                  <div className="col-12 col-md-4 border-end queryInfo">
+                                  <div className="dropdown">
+                                          <div className="d-flex flex-column" role="button" data-bs-toggle="dropdown">
+                                              <p className="topText">From</p>
+                                              <p className="middleText fs-2 fw-semibold">Delhi</p>
+                                              <p className="dayText">New Delhi Railway Station</p>
+                                          </div>
+                                          <ul className="dropdown-menu w-100 drp">
+                                              <li><a className="dropdown-item" href="#">Delhi</a></li>
+                                              <li><a className="dropdown-item" href="#">Mumbai</a></li>
+                                              <li><a className="dropdown-item" href="#">Kolkata</a></li>
+                                              <li><a className="dropdown-item" href="#">Asansol</a></li>
+                                              <li><a className="dropdown-item" href="#">Dhanbad</a></li>
+                                          </ul>
                                       </div>
                                   </div>
-                                  <div className="col-12 col-md-2 border-end" id ="Test">
+                                  <div className="col-12 col-md-2 border-end queryInfo" id ="Test">
                                       <div className="d-flex flex-column">
                                          <p className="travelDate">Trave Date</p>
                                          <div className="d-lg-flex" >
-                                            <p className="fs-2 fw-semibold">{schedule.getDate()}</p><p className="h-100 ms-1 mt-3">{schedule.toLocaleString('en-US', { month: 'short' }).toUpperCase()}</p>
-                                            <p className="h-100 ms-1 mt-3">{year}</p>
+                                            <p className="fs-2 fw-semibold">{schedule.getDate()}</p><p className="ms-1 mt-3">{schedule.toLocaleString('en-US', { month: 'short' }).toUpperCase()}</p>
+                                            <p className="ms-1 mt-3">{year}</p>
                                          </div>
                                          
                                           <Flatpickr
@@ -98,13 +109,13 @@ export default function searchAreaDesktop() {
                                                     maxDate: new Date().fp_incr(120)
                                                 }}
                                           />
-                                          <p>Day</p>
+                                          <p className="dayText">Day</p>
                                       </div>
                                   </div>
-                                  <div className="col-12 col-md-2">
+                                  <div className="col-12 col-md-2 queryInfo">
                                       <div className="d-flex flex-column">
                                           <p className="topText">From</p>
-                                          <h3>Delhi</h3>
+                                          <h3 className="middleText">Delhi</h3>
                                           <p>New Delhi</p>
                                       </div>
                                   </div>
